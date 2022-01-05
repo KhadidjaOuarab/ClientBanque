b4576154703d27b5976b68a0a45f1378bd85d461
ouarab.khadidja <ouarab.khadidja@gmail.com>
2022-01-05 21:49:21 +0100

    reducer client banqque

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,50 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Input from './Component/Input'
-
+import { useReducer } from 'react'
 
 function App() {
 
-  const [inputValue1, setInputValue1] = useState(0);
-  const [inputValue2, setInputValue2] = useState(0);
-  const [inputValue3, setInputValue3] = useState(0);
-  const [outputTab, setOutputTab] = useState(0)
+  const [inputValue, setInputValue] = useState(0);
+  const initialState = { outputTab: 0 }
+  const [state, dispatch] = useReducer(reducerFunction, initialState)
   
-  const getValue1 = (e) => {
-    setInputValue1(e.target.value)
 
-  }
-  const getValue2 = (e) => {
-    setInputValue2(e.target.value)
-
-  }
-  const getValue3 = (e) => {
-    setInputValue3(e.target.value)
-
+  const getValue = (d) => {
+    setInputValue(d)
   }
 
-  const printValue = (e) => {
-    e.preventDefault()
 
-    setOutputTab((parseInt(outputTab) + parseInt(inputValue1) + parseInt(inputValue2)) - parseInt(inputValue3))
+  function reducerFunction(state, action) {
+  
+    switch (action.type) {
+      case 'recharge':
+        return { outputTab: state.outputTab + parseInt(inputValue) };
+      case 'virement':
+        return { outputTab: state.outputTab + parseInt(inputValue) };
+      case 'tel':
+        return { outputTab: state.outputTab - parseInt(inputValue) };
+      default:
+      
+        break;
+    }
 
-    setInputValue1(0)
-    setInputValue2(0)
-    setInputValue3(0)
   }
+ 
   return (
     <div>
       <div className='center'>
         <h1>Bienvenue dans l'espace client</h1>
         <h1>de votre banque</h1>
       </div>
-      <p className='right'>votre solde est : {outputTab}</p>
+      <p className='right'>votre solde est : {state.outputTab}</p>
       <h2 className='center'>Nos Services</h2>
 
-    <div className='row'>
-      <Input label='Recharger Votre Compte' printValue={printValue} getValue={getValue1} inputVal={inputValue1} />
-      <Input label='Virement' printValue={printValue} getValue={getValue2} inputVal={inputValue2} />
-      <Input label='Recharge téléphone' printValue={printValue} getValue={getValue3} inputVal={inputValue3} />
-    </div>
+      <div className='row'>
+        <Input label='Recharger Votre Compte' sendValue={getValue} calculate={()=>dispatch({type:"recharge"})} />
+        <Input label='Virement' sendValue={getValue} calculate={()=>dispatch({type:"virement"})} />
+        <Input label='Recharge téléphone' sendValue={getValue} calculate={()=>dispatch({type:"tel"})}  />
+      </div>
     </div>
   )
 }
--- src/Component/Input.jsx
+++ src/Component/Input.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import '../App.css'
-function Input({ label,printValue ,getValue,inputVal}) {
+import { useState } from 'react'
+
+function Input({ label,sendValue, calculate}) {
+    const [inputVal, setInputVal] = useState(0);
+    const getValue = (e) => {
+        setInputVal(parseInt(e.target.value))
+    }
+
+    const  printValue  = (e) => {
+        e.preventDefault()
+        sendValue(inputVal)
+        calculate()
+        setInputVal(0)
+    
+      }
+
+
     return (
         <div>
             <form onSubmit={printValue} className='col' >
